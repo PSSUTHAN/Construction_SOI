d7d483986f639003ca71f3bc7627c585b144753e
Suthanthiran <pssuthanthiran@gmail.com>
2026-09-24 08:40:09 +0530

    initial

---
+++ frontend/frontend/src/pages/BuilderDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ContractorDashboard from './ContractorDashboard';
+
+// Backwards compatibility alias for ContractorDashboard
+const BuilderDashboard = () => {
+    return <ContractorDashboard />;
+};
+
+export default BuilderDashboard;
